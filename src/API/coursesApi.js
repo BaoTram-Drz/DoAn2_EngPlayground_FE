9739ade9d957915de89a528b76a220a9f8413821
NguyenMinhHieu3101 <hieu31012002@gmail.com>
2023-10-12 14:28:15 +0700

    Xong History
    - Bấm nút bắt đầu khóa học ở trang Chi tiết khóa học thì sẽ lưu lại lịch sử gồm tên khóa học, trạng thái học (còn đang tiếp), thời gian.

--- src/API/coursesApi.js
+++ src/API/coursesApi.js
@@ -22,6 +22,19 @@ export const getContinueCourses = async () => {
     throw new Error(error.response.data.message);
   }
 };
+export const getHistoryCourses = async () => {
+  const currentUser = JSON.parse(localStorage.getItem('user'));
+  const user = currentUser._id;
+
+  console.log(user);
+  try {
+    const response = await api.get('/users/getHistoryCourses', { params: { user } });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+};
 
 
 export const getCoursesListen = async () => {
--- src/API/saveUserCourseApi.js
+++ src/API/saveUserCourseApi.js
@@ -9,3 +9,13 @@ export const saveUser_Course = async (user_course) => {
     throw new Error(error.message);
   }
 };
+
+export const saveHistory_Course = async (history_course) => {
+  console.log(history_course);
+try {
+  const response = await api.post('/users/saveHistoryCourse', history_course);
+  return response.data;
+} catch (error) {
+  throw new Error(error.message);
+}
+};
