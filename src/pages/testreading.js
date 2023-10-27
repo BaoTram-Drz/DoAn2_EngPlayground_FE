6086bcfa45f7162b6b30c61273bede01a7d08e87
NguyenMinhHieu3101 <hieu31012002@gmail.com>
2023-10-28 03:17:28 +0700

    Xong 90% cái API lắng nghe
    cài thêm:
    fast-levenshtein
    levenshtein
    react-sound
    
    nhớ có --force
    
    Đại khái là bấm vô cái nút có cái loa sẽ nghe phát âm từ đó, bấm vô nút có cái mic sẽ nói vô đó;
    
    Sau đó nó đánh giá kết quả theo chỉ số levenshtein (chỉ số sai lệch): nếu sai lệch = 0 thì phát âm đúng, chuyển màu xanh, sai lệch , >0 và <= maxSai lệch thì màu vàng, còn lại màu đỏ; có chèn âm thanh
    
    Muốn đổi từ thì đổi chỗ result[1]

--- src/pages/testreading.js
+++ src/pages/testreading.js
@@ -1,56 +1,230 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { FaVolumeUp } from 'react-icons/fa'
-import { CiMicrophoneOn } from "react-icons/ci";
-
-
+import { FaVolumeUp } from 'react-icons/fa';
+import { CiMicrophoneOn } from 'react-icons/ci';
+import fastLevenshtein from 'fast-levenshtein';
+import { getVocab } from '../API/vocabApi';
+import Sound from 'react-sound'; // Import the Sound component
+import successSound from './success.mp3'; // Define your success sound file path
+import warningSound from './err.mp3'; // Define your warning sound file path
+import errorSound from './err.mp3'; // Define your error sound file path
 
 export const Container = styled.div`
   width: 100%;
   height: 100%;
   margin: 10%;
-
 `;
+
 export const Vocabulary = styled.p`
   font: normal 400 2rem 'Autour One';
 `;
+
 const ListButton = styled.div`
-    width: 100%;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
+  width: 100%;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
 `;
+
 const Button = styled.button`
-    width: 20%;
-    padding: 5px 24px;
-    font: normal 400 2rem 'Autour One';
-    color: #ffc24b;
-    background-color: white;
-    border: 3px solid #f47068;
-    border-radius: 14px;
-    text-align: center;
-    text-decoration: none;
+  width: 20%;
+  padding: 5px 24px;
+  font: normal 400 2rem 'Autour One';
+  color: #ffc24b;
+  background-color: white;
+  border: 3px solid #f47068;
+  border-radius: 14px;
+  text-align: center;
+  text-decoration: none;
 `;
-const ListenButton = styled(Button)`
 
-`;
-const SpeakButton = styled(Button)`
-
-`;
+const ListenButton = styled(Button)``;
+const SpeakButton = styled(Button)``;
+const StopButton = styled(Button)``;
 const SubmitButton = styled(Button)`
-    margin-top: 5%;
+  margin-top: 5%;
 `;
+
+const FeedbackContainer = styled.div`
+  background-color:#fef0f1;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #f00;
+  margin-top: 10px;
+  border-color: ${(props) => props.color || '#fef0f1'};
+`;
+
+const RedText = styled.span`
+  color: red;
+`;
+
+const OrangeText = styled.span`
+  color: orange;
+`;
+
+const GreenText = styled.span`
+  color: green;
+`;
+
 function TestReading() {
-    return ( 
-        <Container>  
-            <h1>Test Reading</h1>
-            <Vocabulary> Vocabulary</Vocabulary>
-            <ListButton>
-                <ListenButton> <FaVolumeUp/> </ListenButton>
-                <SpeakButton> <CiMicrophoneOn/></SpeakButton>
-            </ListButton>
-            <SubmitButton> Submit</SubmitButton>
-            <p> lời nhận xét</p>
-        </Container>
-     );
+  const [wordTest, setWordTest] = useState({});
+  const [playSuccess, setPlaySuccess] = useState(false);
+  const [playWarning, setPlayWarning] = useState(false);
+  const [playError, setPlayError] = useState(false);
+  const [feedbackColor, setFeedbackColor] = useState(''); // Define the feedbackColor state
+
+  useEffect(() => {
+    const getVocabulary = async () => {
+      try {
+        const topicCourse = { topic: 'animals' };
+        const result = await getVocab(topicCourse);
+        setWordTest(result[1]);
+
+        console.log('wordTest:', wordTest);
+
+        // Optionally set the vocabulary data in your component state
+        // setVocabulary(result);
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    };
+    getVocabulary();
+  }, []);
+
+  const referenceWord = 'hello';
+  const maxDistance = 10;
+
+  const [feedback, setFeedback] = useState('');
+  const [listening, setListening] = useState(false);
+  const [userInput, setUserInput] = useState('');
+
+  const handleSpeechRecognition = () => {
+    if (listening) {
+      return;
+    }
+
+    if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
+      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+      recognition.interimResults = true;
+
+      recognition.onstart = () => {
+        setListening(true);
+        setFeedback('Microphone is on. Speak now.');
+      };
+
+      recognition.onend = () => {
+        setListening(false);
+        console.log('Microphone is off.');
+      };
+
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+
+        // Only proceed if the user input is different
+        if (transcript !== userInput) {
+          setUserInput(transcript);
+
+          const distance = fastLevenshtein.get(wordTest.name, transcript);
+
+          if (distance === 0) {
+            setPlaySuccess(true);
+            setFeedback(
+              <div>
+                <span>Original IPA: <GreenText>{getOriginalIPA(wordTest)}</GreenText></span>
+                <br />
+                <span>Your IPA: <GreenText>{transcript}</GreenText></span>
+                <br />
+                <span>Levenshtein distance: {distance}</span>
+              </div>
+            );
+            setFeedbackColor('green'); // Set feedbackColor to green
+          } else if (distance <= maxDistance) {
+            setPlayWarning(true);
+            setFeedback(
+              <div>
+                <span>Original IPA: <OrangeText>{getOriginalIPA(wordTest)}</OrangeText></span>
+                <br />
+                <span>Your IPA: <OrangeText>{transcript}</OrangeText></span>
+                <br />
+                <span>Levenshtein distance: {distance}</span>
+              </div>
+            );
+            setFeedbackColor('orange'); // Set feedbackColor to orange
+          } else {
+            setPlayError(true);
+            setFeedback(
+              <div>
+                <span>Original IPA: <RedText>{getOriginalIPA(wordTest)}</RedText></span>
+                <br />
+                <span>Your IPA: <RedText>{transcript}</RedText></span>
+                <br />
+                <span>Levenshtein distance: {distance}</span>
+              </div>
+            );
+            setFeedbackColor('red'); // Set feedbackColor to red
+          }
+        }
+      };
+
+      recognition.start();
+    } else {
+      console.log('Speech recognition not supported in this browser.');
+    }
+  };
+
+  const handleListening = (word) => {
+    if ('speechSynthesis' in window) {
+      const utterance = new SpeechSynthesisUtterance(word);
+      window.speechSynthesis.speak(utterance);
+    } else {
+      console.error('This browser does not support the SpeechSynthesis API.');
+    }
+  };
+
+  const getOriginalIPA = (word) => {
+    return word.sound;
+  };
+
+  useEffect(() => {
+    if ('Notification' in window) {
+      if (Notification.permission !== 'granted') {
+        Notification.requestPermission();
+      }
+    }
+  }, []);
+
+  return (
+    <Container>
+      <h1>Test Reading</h1>
+      <Vocabulary> {wordTest.name}</Vocabulary>
+      <ListButton>
+        <ListenButton onClick={() => handleListening(wordTest.name)}> <FaVolumeUp /> </ListenButton>
+        <SpeakButton onClick={handleSpeechRecognition}> <CiMicrophoneOn /></SpeakButton>
+      </ListButton>
+      <SubmitButton> Submit</SubmitButton>
+      <FeedbackContainer color={feedbackColor}>
+        <p>{feedback}</p>
+      </FeedbackContainer>
+
+      <Sound
+        url={successSound}
+        playStatus={playSuccess ? Sound.status.PLAYING : Sound.status.STOPPED}
+        onFinishedPlaying={() => setPlaySuccess(false)}
+      />
+
+      <Sound
+        url={warningSound}
+        playStatus={playWarning ? Sound.status.PLAYING : Sound.status.STOPPED}
+        onFinishedPlaying={() => setPlayWarning(false)}
+      />
+
+      <Sound
+        url={errorSound}
+        playStatus={playError ? Sound.status.PLAYING : Sound.status.STOPPED}
+        onFinishedPlaying={() => setPlayError(false)}
+      />
+    </Container>
+  );
 }
 
 export default TestReading;
